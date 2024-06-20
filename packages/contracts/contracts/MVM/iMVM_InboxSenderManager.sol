fd218e87be27bf62ee46a5a0539f235091fb88b5
imetheric <im.etheric@gmail.com>
2024-06-20 15:32:56 +0800

    dlt: expose defaultInboxSender function for MVM_InboxSenderManager interface

--- packages/contracts/contracts/MVM/iMVM_InboxSenderManager.sol
+++ packages/contracts/contracts/MVM/iMVM_InboxSenderManager.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.9;
 interface iMVM_InboxSenderManager {
     event InboxSenderSet(uint256 indexed blockNumber, address indexed inboxSender);
 
+    function defaultInboxSender() external view returns (address);
+
     function setInboxSender(uint256 blockNumber, address inboxSender) external;
-    
+
     function getInboxSender(uint256 blockNumber) external view returns (address);
 }
